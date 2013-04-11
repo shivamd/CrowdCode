5bff71a921d020a12375745ebe0f4a7de2447c4d
Shivam Daryanani <daryananis@gmail.com>
2013-04-11 10:34:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdcode::Application.config.secret_token = '18ff5886f5257fbfb2a97e8786b4a3e2410958b1b13d243bcd8715537e0a64000dca7ce1dcc7240ce0cc59641dd6e9fe2f0fd78851e73dd92ce4071bb09aaa00'
