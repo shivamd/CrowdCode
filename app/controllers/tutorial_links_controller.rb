854da46ed60afdfc4dfceda88eac40199bd0bc52
Shivam Daryanani <daryananis@gmail.com>
2013-04-11 14:21:36 -0700

    TutorialLink with user integration

--- app/controllers/tutorial_links_controller.rb
+++ app/controllers/tutorial_links_controller.rb
@@ -1,10 +1,14 @@
 class TutorialLinksController < ApplicationController
+
+	# before_filter :authenticate_user!, only: [:new, :create]
+
 	def new
 		@tut_link = TutorialLink.new
 	end
 
 	def create
 		@tut_link = TutorialLink.new(params[:tutorial_link])
+		@tut_link.user_id = current_user.id
 		if @tut_link.save 
 			redirect_to tutorial_links_path, notice: "Submitted Tutorial, it will be shortly uploaded"
 		else
