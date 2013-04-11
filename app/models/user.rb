bdf2c89a758611c320bfb8fa8b7d61cf38c33277
Matthew matthewhaguemh@gmail.com <matthewhaguemh+m@gmail.com>
2013-04-11 14:20:57 -0700

    log in and out with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
  
 end
