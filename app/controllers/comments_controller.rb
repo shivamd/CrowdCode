bed02538130f191b8d02b70d51c1dacd1adba5b0
Shivam Daryanani <daryananis@gmail.com>
2013-04-12 16:44:08 -0700

    Edit comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,20 @@
 class CommentsController < ApplicationController
-  def new
-    
-  end
 
   def create
     @comment = Comment.new(params[:comment])
     @comment.user_id = current_user.id
     @tutorial = @comment.get_tutorial
     @comment.save ? redirect_to(@tutorial) : (redirect_to :back, notice: "failed to created comment")
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update 
+    @comment = Comment.find(params[:id])
+    @comment.update_attributes(params[:comment])
+    @tutorial = @comment.get_tutorial 
+    @comment.save ? redirect_to(@tutorial) : (render :edit, notice: "failed to update comment")
   end
 end
