be608f4c461899c86b40f678e0125f17b8dddeac
Matthew <matthewhaguemh@gmail.com>
2013-04-13 16:41:36 -0700

    Put search form in navbar and make it toggleable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-   @user =  User.includes(:tutorials, 
+   @user = User.includes(:tutorials, 
     							:tutorial_links,
     							:comments).find_by_username(params[:username])
   end
