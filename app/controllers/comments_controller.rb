c15aa026bf1184ca1ff04fa06cf377f8a0269a2f
Matthew <matthewhaguemh@gmail.com>
2013-04-12 20:15:05 -0700

    iron out small bug in formatting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,10 @@
 class CommentsController < ApplicationController
-  respond_to :js, :html
+  
   def create
-    @comments = Comment.all
     @comment = Comment.new(params[:comment])
     @comment.user_id = current_user.id
     @tutorial = @comment.get_tutorial
     @comment.save
-    # respond_to do |format|
-    #   format.js { render :json => comment }
-    # end
-    # comment.save ? redirect_to(@tutorial) : (redirect_to :back, notice: "failed to created comment")
-
   end
 
   def edit
