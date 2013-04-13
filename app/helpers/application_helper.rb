ff7b812b0cefc9d742adee4b5e88a4b3f011278f
Shivam Daryanani <daryananis@gmail.com>
2013-04-12 23:34:18 -0700

    fixed gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
-  def gravatar_for(email)
+  def gravatar_for(email, options={})
     gravatar_id = Digest::MD5::hexdigest(email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{size}"
     image_tag(gravatar_url, alt: email, class: "gravatar")
   end
 
