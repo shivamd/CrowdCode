7cbea3bd764f8c35729c07a36c70bd6b7e070e14
Shivam Daryanani <daryananis@gmail.com>
2013-04-13 16:14:07 -0700

    Ability to see recent votes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
   def show
-  	active_user = params[:username] || current_user.username
+   active_user = params[:username] || current_user.username
    @user=  User.includes(:tutorials, 
     							:tutorial_links,
-    							:comments).find_by_username(active_user)
+    							:comments, :votes).find_by_username(active_user)
   end
 end
