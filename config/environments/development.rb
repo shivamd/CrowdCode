485870a04cc3602470d5961b6533f28ea13005a3
Matthew <matthewhaguemh@gmail.com>
2013-04-13 20:34:14 -0700

    mailer sends in console but not to actual email WIP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Crowdcode::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.mailer = "UserMailer"
   # Added default url for devise
   config.action_mailer_default_url_options = { :host => 'localhost:3000' }
