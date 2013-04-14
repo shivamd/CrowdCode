3476a651c15e85c764a6bf684e31090eedadfac1
Matthew <matthewhaguemh@gmail.com>
2013-04-13 20:53:18 -0700

    COMMENTED OUT MAILERS FOR NOW...CONNECTION ERROR.  WIP. TREAD CAREFULLY

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :description
-  after_save :send_welcome_email
+  # after_save :send_welcome_email
   has_many :tutorial_links
   has_many :tutorials 
   has_many :comments
@@ -53,8 +53,8 @@ class User < ActiveRecord::Base
     end
   end
 
-  def send_welcome_email
-    UserMailer.welcome_email(self).deliver
-  end
+  # def send_welcome_email
+  #   UserMailer.welcome_email(self).deliver
+  # end
 
 end
