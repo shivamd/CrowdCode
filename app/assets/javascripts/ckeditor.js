31c6efa0bc158a290944b8238716f8b6a63752ec
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-04-15 13:40:47 -0700

    ckeditor formatting on tutorial_new

--- app/assets/javascripts/ckeditor.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-   if ($('textarea.ckeditor').length > 0) {       
-     var data = $('textarea.ckeditor');
-     $.each(data, function(i) {
-       CKEDITOR.replace(data[i].id);
-     });     
-   }  
-});
