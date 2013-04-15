9b37f2b062874e88626b4927ff2e2132623cb55b
Shivam Daryanani <daryananis@gmail.com>
2013-04-15 10:43:08 -0700

    front page search bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
 
   def index
   	@tutorials = Tutorial.joins(:category).page(params[:page]).per_page(8)
+  	@search = Tutorial.search(params[:q])
+  	redirect_to tutorials_url unless params[:q].nil?
   end
 
   def about
