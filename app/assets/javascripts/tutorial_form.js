c4341719b02c8d21890ed1ad23dd54bf34ca51c8
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-04-15 08:56:46 -0700

    add ckeditor to tutorial_new, still needs formatting

--- app/assets/javascripts/tutorial_form.js
+++ app/assets/javascripts/tutorial_form.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('div.tutorial_content').hide();
+	// $('div.tutorial_content').hide();
 	$('#write_a_tutorial').click(function(){
 		$('div.tutorial_content').toggle();
 		$('div.tutorial_url').toggle();
