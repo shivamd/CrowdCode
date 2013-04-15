c6cbadf5486932c1f3c8c6273ee6ffe9f89f6184
Apprentice <apprentice@devbootcamp.com>
2013-04-15 10:39:54 -0700

    tabs working

---
+++ app/assets/javascripts/tabs.js
@@ -0,0 +1,12 @@
+$(document).ready(function (){
+    $('.tabs li a').click(function (e) {
+        e.preventDefault();
+        var $tab = $(this).parent();
+        var $other_tabs = $tab.siblings();
+        var tab_content = $(this).attr("href");
+        $('.tab').hide();
+        $(tab_content).show();
+        $tab.addClass('active');
+        $other_tabs.removeClass('active');
+    });
+  });
