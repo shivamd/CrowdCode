df05a16bfe163a5c192a1fc6a2027a8fd328494e
Apprentice <apprentice@devbootcamp.com>
2013-04-16 09:50:45 -0700

    validate uniqueness of username added to model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ describe User do
   it { should have_many(:tutorials) }
   it { should have_many(:votes)}
   it { should have_many(:bookmarks) }
+  it { should validate_uniqueness_of(:username)}
 end
