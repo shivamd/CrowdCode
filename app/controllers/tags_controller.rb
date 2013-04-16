534f481aaa613c29fb88d69f2d85776091a4b9ae
Apprentice <apprentice@devbootcamp.com>
2013-04-16 10:48:48 -0700

    remove search from tag page for now

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,9 @@
 class TagsController < ApplicationController
 
 	def index 
-    @search = Tag.search(params[:q])
-    @tags = @search.result 
 		@tags = Tag.all.sort {|a,b| b.taggings.count <=> a.taggings.count }
     @split_tags = @tags.each_slice(4).to_a
+   
 	end
 
 	def show 
