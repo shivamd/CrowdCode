681d58124f38aab63a18f17ca06aae4f48b2a7ba
Apprentice <apprentice@devbootcamp.com>
2013-04-16 16:58:37 -0700

    downcase all tags before create

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,6 @@ class TagsController < ApplicationController
 	def index 
 		@tags = Tag.all.sort {|a,b| b.taggings.count <=> a.taggings.count }
     @split_tags = @tags.each_slice(4).to_a
-   
 	end
 
 	def show 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,10 @@ class Tag < ActiveRecord::Base
 
   has_many :tutorials, through: :taggings, source: :taggable, source_type: "Tutorial"
   has_many :taggings
-
+  before_create :sanitize
   validates :content, presence: true, format: { with: /^[\S]*$/ }
+
+  def sanitize
+    self.content.downcase! if self.content =~ /[A-Z]/
+  end
 end
