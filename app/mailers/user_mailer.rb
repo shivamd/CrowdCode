38cfc622a7904fd55f0eb19a15b550bc5c654320
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-04-17 01:41:21 -0700

    comment out all mailer logic, breaks signup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
-class UserMailer < Devise::Mailer
-  default from: "noreply@example.com"
+# class UserMailer < Devise::Mailer
+#   default from: "noreply@example.com"
 
-   def welcome_email(user)
-    @user = user
-    @url  = "http://stormy-hollows-8884.herokuapp.com"
-    mail(:to => user.email, :subject => "Welcome to CrowdCode")
-  end
+#    def welcome_email(user)
+#     @user = user
+#     @url  = "http://stormy-hollows-8884.herokuapp.com"
+#     mail(:to => user.email, :subject => "Welcome to CrowdCode")
+#   end
 
-end
+# end
