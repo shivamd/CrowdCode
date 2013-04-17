27739b454efc3473bea362276509eebb88f756f8
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-04-16 18:17:07 -0700

    change code block syntax highlighting to tomorrow-night theme

--- app/assets/javascripts/highlightjs.js
+++ app/assets/javascripts/highlightjs.js
@@ -1,15 +1,12 @@
 $(document).ready(function(){
+  // Loads highlight.js
   hljs.initHighlightingOnLoad();
+
   // This allows highlight.js to work properly
   $('pre').each(function(x,y){
     var code_block_text = $(y).text();
-    // if (code_block_text == undefined){
-    //   var code_block_text = $(y).html();
-    // }; 
     var language = $(y).attr("data-language");
-    $(y).html("<code class="+ language +">"+ code_block_text +"</code>");
+    $(y).html("<code class=" + language + ">" + code_block_text + "</code>");
   });
-  
-  //This accounts for highlight.js when 
 });
 
