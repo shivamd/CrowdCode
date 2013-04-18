00124fb1e2187d95a9f9db41c8f199f934a40a9b
Apprentice <apprentice@devbootcamp.com>
2013-04-18 13:51:28 -0700

    add padding to individual span elements in word cloud

--- app/assets/javascripts/word_cloud.js
+++ app/assets/javascripts/word_cloud.js
@@ -1,5 +1,8 @@
 WordCloud = {
 	init: function(wordList) {
-		$("#wordcloud").jQCloud(wordList).draggable();
+		$("#wordcloud").jQCloud(wordList, {
+      width: 300,
+      height: 200
+    })
 	}
 }
